fbe1add39acadfa706cefe51d36bd9db0e8938d3
echochio <chio@nzgft.com>
2017-03-09 11:12:12 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,11 +5,18 @@ docker run -d -P  --privileged=true --name=pcmk_test1 pacemaker_docker
 docker run -d -P  --privileged=true --name=pcmk_test2 pacemaker_docker
 docker run -d -P  --privileged=true --name=pcmk_test3 pacemaker_docker
 
+echo Sleep 40 sec wait for Cluster start
+sleep 40
 
 echo Start pcs auth
 docker exec -it pcmk_test1 pcs cluster auth 172.17.0.3 172.17.0.4 172.17.0.5 -u hacluster -p hacluster
 
+echo copy 3 node config file
+docker exec -it pcmk_test1 cp /etc/corosync/corosync-node3.conf /etc/corosync/corosync.conf
+docker exec -it pcmk_test2 cp /etc/corosync/corosync-node3.conf /etc/corosync/corosync.conf
+docker exec -it pcmk_test3 cp /etc/corosync/corosync-node3.conf /etc/corosync/corosync.conf
+
 echo Restart Cluster
-docker ecec -it pcmk_test1 /usr/sbin/pcmk_restart.sh
-docker ecec -it pcmk_test2 /usr/sbin/pcmk_restart.sh
-docker ecec -it pcmk_test3 /usr/sbin/pcmk_restart.sh
+docker exec -it pcmk_test1 /usr/sbin/pcmk_restart.sh &
+docker exec -it pcmk_test2 /usr/sbin/pcmk_restart.sh &
+docker exec -it pcmk_test3 /usr/sbin/pcmk_restart.sh &
