732b479efd9e6a61e82ae64187049ad3f7d7cef2
echochio <chio@nzgft.com>
2017-03-09 14:10:31 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -20,6 +20,6 @@ echo Start pcs auth again
 docker exec -it pcmk_test1 pcs cluster auth 172.17.0.3 172.17.0.4 172.17.0.5 -u hacluster -p hacluster
 
 echo Restart Cluster
-docker exec -it pcmk_test1 /usr/sbin/pcmk_restart.sh &
-docker exec -it pcmk_test2 /usr/sbin/pcmk_restart.sh &
-docker exec -it pcmk_test3 /usr/sbin/pcmk_restart.sh &
+docker exec -it pcmk_test1 sh /usr/sbin/pcmk_restart.sh &
+docker exec -it pcmk_test2 sh /usr/sbin/pcmk_restart.sh &
+docker exec -it pcmk_test3 sh /usr/sbin/pcmk_restart.sh &
