2a65b5b00da0bdc6295b85a936eae3c28912824b
echochio <chio@nzgft.com>
2017-03-09 10:44:57 +0800

    Update pcmk_launch.sh

--- helper_scripts/pcmk_launch.sh
+++ helper_scripts/pcmk_launch.sh
@@ -1,117 +1,41 @@
 #!/bin/bash
 
-nodelist1="$NODE1"
-nodelist2="$NODE2"
-nodelist3="$NODE3"
-pcemakerstop="$PCE"
-
-trap "stop; exit 0;" SIGTERM SIGINT 
-
-status()
-{
-	prog="$1"
-	if [ -z "$prog" ]; then
-		prog="pacemakerd"
-	fi
-
-	pid=$(pidof $prog 2>/dev/null)
-	rtrn=$?
-	if [ $rtrn -ne 0 ]; then
-		echo "$prog is stopped"
-	else
-		echo "$prog (pid $pid) is running..."
-	fi
-	return $rtrn
-}
-
 start()
 {
+        if [ ! -n "$IP" ]; then
+                export IP=`ip addr |grep "scope global eth0"|sed 's/    inet //g'|sed 's/\/16 scope global eth0//g'`
+                sed -i 's/127.0.0.1/'$(echo $IP)'/g' /etc/corosync/corosync.conf
+                sed -i 's/Listen 80/Listen '$(echo $IP)':80/g' /etc/httpd/conf/httpd.conf
+                mkdir -p /etc/systemd/system-preset/
+                echo 'enable pcsd.service' > /etc/systemd/system-preset/00-pcsd.preset
+                systemctl enable pcsd
+        fi
 
-	if [ -n "$nodelist1" ]; then
-		cp /etc/corosync/corosync-node3.conf /etc/corosync/corosync.conf
-		sed -i 's/172.17.0.2/'$(echo $nodelist1)'/g' /etc/corosync/corosync.conf
-		sed -i 's/172.17.0.3/'$(echo $nodelist2)'/g' /etc/corosync/corosync.conf
-		sed -i 's/172.17.0.4/'$(echo $nodelist3)'/g' /etc/corosync/corosync.conf
-	fi
-	
-	if [ ! -n "$IP" ]; then
-		export IP=`ip addr |grep "scope global eth0"|sed 's/    inet //g'|sed 's/\/16 scope global eth0//g'`
-		sed -i 's/127.0.0.1/'$(echo $IP)'/g' /etc/corosync/corosync.conf
-		sed -i 's/Listen 80/Listen '$(echo $IP)':80/g' /etc/httpd/conf/httpd.conf
-		mkdir -p /etc/systemd/system-preset/
-		echo 'enable pcsd.service' > /etc/systemd/system-preset/00-pcsd.preset
-		systemctl enable pcsd
-	fi
-		
-	if [ -n "$nodelist" ]; then
-		pcs cluster setup --force --local --name k8master $nodelist
-	fi
-	rm -f /usr/local/apache2/logs/httpd.pid
+        rm -f /usr/local/apache2/logs/httpd.pid
 
-	/sbin/httpd -DFOREGROUND &
-	/usr/lib/systemd/systemd --system & 
-	
-	sleep 30
-	
-	if [ ! -n "$pcemakerstop" ]; then
-	
-		/usr/share/corosync/corosync start > /dev/null 2>&1
-		mkdir -p /var/run
+        /sbin/httpd -DFOREGROUND &
+        /usr/lib/systemd/systemd --system &
 
-		export PCMK_debugfile=/var/log/pacemaker.log
-		(pacemakerd &) & > /dev/null 2>&1
-		sleep 5
+        sleep 30
 
-		pid=$(pidof pacemakerd)
-		if [ "$?" -ne 0 ]; then
-			echo "startup of pacemaker failed"
-			exit 1
-		fi
-		echo "$pid" > /var/run/pacemakerd.pid
-	fi
-}
+        /usr/share/corosync/corosync start > /dev/null 2>&1
+        mkdir -p /var/run
 
-stop()
-{
-	desc="Pacemaker Cluster Manager"
-	prog="pacemakerd"
-	shutdown_prog=$prog
+        export PCMK_debugfile=/var/log/pacemaker.log
+        (pacemakerd &) & > /dev/null 2>&1
+        sleep 5
 
-	if ! status $prog > /dev/null 2>&1; then
-	    shutdown_prog="crmd"
-	fi
-
-	cname=$(crm_node --name)
-	crm_attribute -N $cname -n standby -v true -l reboot
-
-	if status $shutdown_prog > /dev/null 2>&1; then
-	    kill -TERM $(pidof $prog) > /dev/null 2>&1
-
-	    while status $prog > /dev/null 2>&1; do
-		sleep 1
-		echo -n "."
-	    done
-	else
-	    echo -n "$desc is already stopped"
-	fi
-
-	rm -f /var/lock/subsystem/pacemaker
-	rm -f /var/run/${prog}.pid
-
-	/usr/share/corosync/corosync stop > /dev/null 2>&1
-	killall -q -9 'corosync'
-	killall -q -9 'crmd stonithd attrd cib lrmd pacemakerd corosync'
+        pid=$(pidof pacemakerd)
+        if [ "$?" -ne 0 ]; then
+                echo "startup of pacemaker failed"
+                exit 1
+        fi
+        echo "$pid" > /var/run/pacemakerd.pid
 }
 
 start
 
 while true; do
-	if [ ! -n "$pcemakerstop" ]; then
-		status "pacemakerd" || exit 1
-		status "corosync" || exit 1
-	fi
-	sleep 5
 done
 
 exit 0
-#TODO trap SIGTERM and do a graceful shutdown
